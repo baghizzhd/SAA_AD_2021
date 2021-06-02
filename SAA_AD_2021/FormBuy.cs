7674aae3df5f79b9510fdcff1522b42ba9325cf3
baghiz zuhdi adzin <baghizzhd@gmail.com>
2021-06-02 21:56:20 +0700

    p

--- SAA_AD_2021/FormBuy.cs
+++ SAA_AD_2021/FormBuy.cs
@@ -16,10 +16,6 @@ namespace SAA_AD_2021
         {
             InitializeComponent();
         }
-        string a = "";
-        private void button1_Click(object sender, EventArgs e)
-        {
-
-        }
+      
     }
 }
