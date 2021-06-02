ce2068e446c75711f0300e3ac4e556e442151e93
baghiz zuhdi adzin <baghizzhd@gmail.com>
2021-06-02 22:00:00 +0700

    a

--- SAA_AD_2021/FormBuy.Designer.cs
+++ SAA_AD_2021/FormBuy.Designer.cs
@@ -29,13 +29,25 @@ namespace SAA_AD_2021
         /// </summary>
         private void InitializeComponent()
         {
+            this.button1 = new System.Windows.Forms.Button();
             this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(160, 127);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "button1";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
             // 
             // FormBuy
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(928, 580);
+            this.Controls.Add(this.button1);
             this.Name = "FormBuy";
             this.Text = "FormBuy";
             this.ResumeLayout(false);
@@ -43,5 +55,7 @@ namespace SAA_AD_2021
         }
 
         #endregion
+
+        private System.Windows.Forms.Button button1;
     }
 }
--- SAA_AD_2021/FormBuy.cs
+++ SAA_AD_2021/FormBuy.cs
@@ -16,5 +16,10 @@ namespace SAA_AD_2021
         {
             InitializeComponent();
         }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            string a;
+        }
     }
 }
